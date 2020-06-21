38ba8674f246e1ae5a119dbc01b07823e039e4cb
Quentin Gibson <qgibson3@students.kennesaw.edu>
2020-06-21 11:13:06 -0400

    Fix: Remove extra div ending from topnav

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -69,6 +69,6 @@ class SubjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def subject_params
-      params.require(:subject).permit(:grade, :name, :task)
+      params.require(:subject, :name).permit(:grade, :name, :task)
     end
 end
