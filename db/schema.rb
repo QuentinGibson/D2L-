1c3e4c59004ad99da9cc240ff2565c09350b3c2b
Quentin Gibson <qgibson3@students.kennesaw.edu>
2020-07-14 09:54:04 -0400

    add migration from notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_172524) do
+ActiveRecord::Schema.define(version: 2020_07_14_134620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_07_12_172524) do
     t.string "name"
     t.string "grade"
     t.bigint "user_id"
+    t.integer "average"
     t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_07_12_172524) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "tags_tasks", id: false, force: :cascade do |t|
@@ -85,11 +87,17 @@ ActiveRecord::Schema.define(version: 2020_07_12_172524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.bigint "user_id"
+    t.string "grade"
+    t.float "gpa"
+    t.integer "total"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_users_on_user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "subjects", "users"
   add_foreign_key "tasks", "subjects"
+  add_foreign_key "users", "users"
 end
