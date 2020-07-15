149bcfebf87e9175f344f83e2a3b67cc7dc73bf2
Quentin Gibson <qgibson3@students.kennesaw.edu>
2020-07-15 05:03:33 -0400

    route to calendar

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def calendar
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :tags
   resources :subjects do
   resources :tasks
+  match '/calendar' => 'pages#calendar'
+  match '/about' => 'pages#about'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'subjects#index'
